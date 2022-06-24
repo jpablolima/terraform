3cda72bef78b4097495086196ff22719c909b807
João Pablo <jp.pablolima@gmail.com>
2022-06-24 18:33:50 -0300

    feature(*): ec2-aws

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+  region = "us-east-1"
+  # version = "~> 3.0"
+}
+
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "terraform-pablo"
+    key    = "terraform-goku-tfstate"
+    region = "us-east-1"
+  }
+}
